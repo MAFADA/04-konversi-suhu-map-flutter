06cec3b1b7299ae48b03c2c339da107387db57f1
MAFADA <afadanursaiva2001@gmail.com>
2022-03-10 09:40:50 +0700

    Tugas Selesai

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,10 @@ class _MyAppState extends State<MyApp> {
   double _inputUser = 0;
   double _kelvin = 0;
   double _reamur = 0;
+  var listItem = ["Kelvin", "Reamur"];
+  String _newValue = "Kelvin";
+  double _result = 0;
+  List<String> listViewItem = <String>[];
 
   final etInputUser = TextEditingController();
 
@@ -42,26 +46,53 @@ class _MyAppState extends State<MyApp> {
         body: Container(
           margin: const EdgeInsets.all(8),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Input(etInputUser: etInputUser),
-              Result(kelvin: _kelvin, reamur: _reamur),
-              Container(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
+              DropdownButton<String>(
+                items: listItem.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                value: _newValue,
+                onChanged: (String? changeValue) {
+                  setState(() {
+                    _newValue = changeValue!;
                     _konversiSuhu();
-                  },
-                  child: const Text(
-                    "Konversi Suhu",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.lightBlue,
+                  });
+                },
+              ),
+              Result(
+                result: _result,
+              ),
+              SizedBox(
+                child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _konversiSuhu();
+                    },
+                    child: const Text(
+                      "Konversi Suhu",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.lightBlue,
+                    ),
                   ),
                 ),
               ),
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                child: Text(
+                  "Riwayat Konversi",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Expanded(child: RiwayatKonversi(listViewItem: listViewItem)),
             ],
           ),
         ),
@@ -70,58 +101,72 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _konversiSuhu() {
-    _inputUser = double.parse(etInputUser.text);
     setState(() {
-      _reamur = (4 / 5) * _inputUser;
-      _kelvin = _inputUser + 273;
+      _inputUser = double.parse(etInputUser.text);
+      switch (_newValue) {
+        case "Kelvin":
+          {
+            _result = _inputUser + 273;
+            listViewItem.add("Kelvin : " "$_result");
+          }
+          break;
+
+        case "Reamur":
+          {
+            _result = (4 / 5) * _inputUser;
+            listViewItem.add("Reamur : " "$_result");
+          }
+          break;
+      }
     });
+  }
+}
+
+class RiwayatKonversi extends StatelessWidget {
+  const RiwayatKonversi({
+    Key? key,
+    required this.listViewItem,
+  }) : super(key: key);
+
+  final List<String> listViewItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: listViewItem.length,
+        itemBuilder: ((context, index) {
+          return Container(
+            child: Text(listViewItem[index]),
+          );
+        }));
   }
 }
 
 class Result extends StatelessWidget {
   const Result({
     Key? key,
-    required double kelvin,
-    required double reamur,
-  })  : _kelvin = kelvin,
-        _reamur = reamur,
-        super(key: key);
+    required this.result,
+  }) : super(key: key);
 
-  final double _kelvin;
-  final double _reamur;
+  final double result;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 150),
-      child: Row(children: <Widget>[
-        Expanded(
-          child: Column(
-            children: [
-              const Text('Suhu dalam Kelvin'),
-              Text(
-                '$_kelvin',
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              )
-            ],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Hasil",
+            style: TextStyle(fontSize: 20),
           ),
-        ),
-        Expanded(
-          child: Column(
-            children: [
-              const Text('Suhu dalam Reamur'),
-              Text(
-                _reamur.toStringAsFixed(0),
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              )
-            ],
+          Text(
+            result.toStringAsFixed(1),
+            style: TextStyle(fontSize: 30),
           ),
-        )
-      ]),
+        ],
+      ),
     );
   }
 }
